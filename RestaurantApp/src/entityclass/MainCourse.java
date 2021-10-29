a45a1531909c206d3f3b155137ce6fddf9ab8c95
svenkneegear <phengkhai@gmail.com>
2021-10-29 21:33:22 +0800

    conflict test

--- RestaurantApp/src/entityclass/MainCourse.java
+++ RestaurantApp/src/entityclass/MainCourse.java
@@ -3,4 +3,7 @@ package entityclass;
 import java.io.Serializable;
 
 public class MainCourse extends MenuItem implements Serializable {
+    public MainCourse(String mcName, double mcPrice, String mcDescription, String mcID) {
+        super(mcName,mcPrice,mcDescription,mcID);
+    }
 }
