3008b0de48a6e624029cb7972f39b18e8c0a78a9
WijayaTimothyLarry <wijayatimothylarry@gmail.com>
2021-11-01 20:32:36 +0800

    Create MenuMgr.java

---
+++ RestaurantApp/src/controllerclass/MenuMgr.java
@@ -0,0 +1,10 @@
+package controllerclass;
+
+import database.Restaurant;
+import entityclass.Menu;
+
+public class MenuMgr {
+   private  Menu restaurantMenu = Restaurant.restaurantMenu;
+
+    public
+}
