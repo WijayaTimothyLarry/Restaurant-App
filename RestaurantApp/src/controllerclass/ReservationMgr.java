3684232596852ade22ba8ec30226139c7c9ad7b8
Sherwinnn <sherwinsamson1@hotmail.com>
2021-11-06 11:48:46 +0800

    Update ReservMgr

--- RestaurantApp/src/controllerclass/ReservationMgr.java
+++ RestaurantApp/src/controllerclass/ReservationMgr.java
@@ -1,108 +1,128 @@
 package controllerclass;
 
 import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 import database.Restaurant;
 import entityclass.Reservation;
 
-public class ReservationMgr {
-
+public class ReservationMgr 
+{
     private TableMgr tableCtrl = new TableMgr();
-    private static ArrayList<Reservation> reservationList = Restaurant.reservationList;
+    private ArrayList<Reservation> reservationList = Restaurant.reservationList;
     private Scanner sc = new Scanner(System.in);
 
-    public ReservationMgr() {
 
+    public ReservationMgr()
+    {
+        
     }
 
-    public void setTableMgr(TableMgr tableCtrl) {
+    public void setTableMgr(TableMgr tableCtrl)
+    {
         this.tableCtrl = tableCtrl;
     }
 
-    public void newReservation() {
-        String newCustomerContactNo;
 
+    public void newReservation()
+    {
+        int newCustomerContactNo;
+        
         System.out.println("Enter Customer's contact Number to make Reservation");
-        newCustomerContactNo = sc.next();
-        while (newCustomerContactNo == newCustomerContactNo) {
-            for (Reservation newReserv : reservationList) {
-                if (newReserv.getCustomerContactNo() == newCustomerContactNo) {
+        newCustomerContactNo = sc.nextInt();
+        while(newCustomerContactNo == (int)newCustomerContactNo)
+        {
+            for(Reservation newReserv : reservationList)  
+            {
+                if(newReserv.getCustomerContactNo() == newCustomerContactNo)
+                {
                     System.out.println("Reservation already exists for this Customer");
                 }
             }
-            break;
+            break;          
         }
 
-        LocalDate newReservDate = checkReservDate();
+        System.out.println("Enter the date and time of reservation in: yyyy-MM-dd HH:mm ");
+        String strDate = sc.nextLine();
+        Calendar newReservDateTime = strToCalendarLong(strDate);
 
-        LocalTime newArrivalTime = checkReservTime();
+        // ? newReservDateTime.set(Calendar.YEAR, Calendar.MONTH, Calendar.DATE, Calendar.HOUR_OF_DAY, Calendar.MINUTE);
 
-        LocalDateTime newDateTime = LocalDateTime.of(newReservDate, newArrivalTime);
 
         int newNoOfPax;
 
-        System.out.println("Enter the total number of people under Reservation");
-        newNoOfPax = sc.nextInt();
-        while (newNoOfPax == (int) newNoOfPax) {
-            ArrayList<Integer> tableNumbers = tableCtrl.getAvailTableNoByPax(newNoOfPax);
+        System.out.println("Enter the total number of people under Reservation"); 
+        newNoOfPax = sc.nextInt();       
+        while(newNoOfPax == (int)newNoOfPax)
+        {
+            ArrayList<Integer>tableNumbers = tableCtrl.getAvailTableNoByPax(newNoOfPax);
 
-            if (tableNumbers.isEmpty()) {
+            if(tableNumbers.isEmpty())
+            {
                 System.out.println("All tables are Reserved");
             }
-
-            else {
+            
+            else
+            {
                 System.out.println("Enter Customer Name:");
                 String newCustomerName = sc.next();
-                Reservation newReserv = new Reservation(newReservDate, newArrivalTime, newCustomerName,
-                        newCustomerContactNo, newNoOfPax, tableNumbers.get(0));
-
+                Reservation newReserv = new Reservation(newReservDateTime, newCustomerName, newCustomerContactNo, newNoOfPax, tableNumbers.get(0));
+                
                 reservationList.add(newReserv);
                 startReservationTimer(newReserv);
-                System.out.println("New Reservation successfully made for Table Number: " + newReserv.getTableNumber());
-
+                System.out.println("New Reservation successfully made for Table Number: " + newReserv.getTableNumber() );                
+                
             }
         }
     }
 
-    private void startReservationTimer(Reservation newReserv) {
 
-    }
-
-    private LocalTime checkReservTime() {
-        return null;
-    }
-
-    private LocalDate checkReservDate() {
-        return null;
-    }
-
-    private void readFileData(File reservationFile) {
-
-    }
-
-    public static Reservation checkCustomeReservation(String phoneNumber) {
-        for (Reservation reservation : reservationList) {
-            if (reservation.getCustomerContactNo().equals(phoneNumber)) {
-                return reservation;
-            }
+    public static Calendar strToCalendarLong(String strDate)
+    {
+        Calendar c = Calendar.getInstance();
+        try
+        {
+            c.setTime(new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(strDate));
+        } catch(ParseException e)
+        {
+            e.printStackTrace();
         }
+        return c;
+    }
+
+
+    private void startReservationTimer(Reservation newReserv) 
+    {
+        
+    }
+
+
+    private LocalDateTime checkReservTime() 
+    {
         return null;
     }
 
-    public static void removeReservation(String phoneNumber) {
-        Iterator<Reservation> itr = reservationList.iterator();
-        while (itr.hasNext()) {
-            Reservation reservation = itr.next();
-            if (reservation.getCustomerContactNo().equals(phoneNumber)) {
-                itr.remove();
-            }
-        }
+    private LocalDateTime checkReservDate() 
+    {
+        return null;
     }
+
+    private void readFileData(File reservationFile) 
+    {
+
+    }
+
+
+    
+
+
 
 }
