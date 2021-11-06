50a201b16e1c5036381402a258b7d8f536782a3b
WijayaTimothyLarry <wijayatimothylarry@gmail.com>
2021-11-06 09:59:11 +0800

    update managers

--- RestaurantApp/src/controllerclass/MenuMgr.java
+++ RestaurantApp/src/controllerclass/MenuMgr.java
@@ -4,87 +4,87 @@ import database.Restaurant;
 import entityclass.*;
 
 public class MenuMgr {
-    private  Menu restaurantMenu = Restaurant.restaurantMenu;
+    private static Menu restaurantMenu = Restaurant.restaurantMenu;
 
-    public void showAppetizer(){
+    public static void showAppetizer() {
         restaurantMenu.printAppeList();
     }
-    
-    public void showMainCourse(){
+
+    public void showMainCourse() {
         restaurantMenu.printMainCourse();
     }
 
-    public void showDessert(){
+    public void showDessert() {
         restaurantMenu.printDessert();
     }
 
-    public void showDrinks(){
+    public void showDrinks() {
         restaurantMenu.printDrinks();
     }
 
-    public void showPackage(){
+    public void showPackage() {
         restaurantMenu.printPackage();
     }
 
-    //Functions to edit menu
-    //1) Adding into menu
-    //Appetizer
-    public void addAppe(Appetizer appe){
+    // Functions to edit menu
+    // 1) Adding into menu
+    // Appetizer
+    public void addAppe(Appetizer appe) {
         restaurantMenu.addintoAppeList(appe);
     }
-    
-    //Main Course
-    public void addMainCourse(MainCourse maincourse){
+
+    // Main Course
+    public void addMainCourse(MainCourse maincourse) {
         restaurantMenu.addintoMainCourseList(maincourse);
     }
 
-    //Dessert
-    public void addDessert(Dessert dessert){
+    // Dessert
+    public void addDessert(Dessert dessert) {
         restaurantMenu.addintoDessertList(dessert);
     }
 
-    //Drinks
-    public void addDrinks(Drinks drinks){
+    // Drinks
+    public void addDrinks(Drinks drinks) {
         restaurantMenu.addintoDrinksList(drinks);
     }
 
-    //Package
-    public void  addPackage(PackageItem packages){
+    // Package
+    public void addPackage(PackageItem packages) {
         restaurantMenu.addintoPackageList(packages);
     }
 
-    //2) Delete item
-    //Appetizer
-    public void removeAppe(String id){
-        if(restaurantMenu.removeforAppeList(id)==-1){
+    // 2) Delete item
+    // Appetizer
+    public void removeAppe(String id) {
+        if (restaurantMenu.removeforAppeList(id) == -1) {
             System.out.println("Item not found");
         }
     }
 
-    //MainCourse
-    public void removeMainCourse(String id){
-        if(restaurantMenu.removeforMainCourseList(id)==-1){
+    // MainCourse
+    public void removeMainCourse(String id) {
+        if (restaurantMenu.removeforMainCourseList(id) == -1) {
             System.out.println("Item not found");
         }
     }
 
-    //Dessert
-    public void removeDessert(String id){
-        if(restaurantMenu.removeforDessertList(id)==-1){
+    // Dessert
+    public void removeDessert(String id) {
+        if (restaurantMenu.removeforDessertList(id) == -1) {
             System.out.println("Item not found");
         }
     }
 
-    //Drinks
-    public void removeDrinks(String id){
-        if(restaurantMenu.removeforDrinksList(id)==-1){
+    // Drinks
+    public void removeDrinks(String id) {
+        if (restaurantMenu.removeforDrinksList(id) == -1) {
             System.out.println("Item not found");
         }
     }
 
-    //Packages
-    public void removePackage(String id){
-        if(restaurantMenu.removeforPackageList(id)==-1){
+    // Packages
+    public void removePackage(String id) {
+        if (restaurantMenu.removeforPackageList(id) == -1) {
             System.out.println("Item not found");
         }
     }
--- RestaurantApp/src/controllerclass/TableMgr.java
+++ RestaurantApp/src/controllerclass/TableMgr.java
@@ -2,12 +2,14 @@ package controllerclass;
 
 import java.util.ArrayList;
 
-public class TableMgr 
-{
+import database.Restaurant;
+import entityclass.Table;
 
-    public ArrayList<Integer> getAvailTableNoByPax(int newNoOfPax) 
-    {
+public class TableMgr {
+    ArrayList<Table> tableList = Restaurant.tableList;
+
+    public ArrayList<Integer> getAvailableTable(int noPax) {
         return null;
     }
-    
+
 }
