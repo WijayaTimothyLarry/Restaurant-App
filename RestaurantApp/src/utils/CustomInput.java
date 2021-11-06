cbe8d164416813eb660a0331a0c6432fc1e2a54d
Wu Jun Hui <junhuihuii@gmail.com>
2021-11-06 14:22:34 +0800

    Update CustomInput.java
    added reservation window function to check 2h before to ensure that reserved table would not be assigned to customers when timing hits 2h before booking time

--- RestaurantApp/src/utils/CustomInput.java
+++ RestaurantApp/src/utils/CustomInput.java
@@ -117,4 +117,19 @@ public class CustomInput {
         System.out.println("  Date (future) :" + futureDateTime.getTime() );  
         return futureDateTime;
     }
+
+    //use when checking if table is available for booking by checking if there are any reservations or diners 2h before timing in booking request
+    public static Calendar reservationWindow(Calendar bookingTime){
+        
+        Calendar t2hbefore = (Calendar) Calendar.getInstance();
+        t2hbefore.set(Calendar.YEAR, bookingTime.get(Calendar.YEAR));
+        t2hbefore.set(Calendar.MONTH, bookingTime.get(Calendar.MONTH));
+        t2hbefore.set(Calendar.DAY_OF_MONTH, bookingTime.get(Calendar.DAY_OF_MONTH));
+        t2hbefore.set(Calendar.HOUR_OF_DAY, bookingTime.get(Calendar.HOUR_OF_DAY - 2)); // set 2h before time to 2h before booking time
+        t2hbefore.set(Calendar.MINUTE, bookingTime.get(Calendar.MINUTE));
+        t2hbefore.set(Calendar.SECOND, bookingTime.get(Calendar.MINUTE));
+
+        System.out.println("Time that is 2h before timing in booking request: " + t2hbefore.getTime());
+        return t2hbefore;
+    }
 }
