f921694b5b789ce023e0361c1a8cec8324440f77
WijayaTimothyLarry <wijayatimothylarry@gmail.com>
2021-11-06 13:37:51 +0800

    Update TableInterface.java

--- RestaurantApp/src/boundaryclass/TableInterface.java
+++ RestaurantApp/src/boundaryclass/TableInterface.java
@@ -1,5 +1,10 @@
 package boundaryclass;
 
 public class TableInterface {
+    private static Scanner scanner = new Scanner(System.in);
+    int choice;
 
+    public static void showOption() {
+
+    }
 }
