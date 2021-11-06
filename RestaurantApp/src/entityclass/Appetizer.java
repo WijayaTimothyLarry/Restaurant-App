66391802da8a787360f32574aa058e24834d2596
WijayaTimothyLarry <wijayatimothylarry@gmail.com>
2021-11-06 17:15:17 +0800

    Update Appetizer.java

--- RestaurantApp/src/entityclass/Appetizer.java
+++ RestaurantApp/src/entityclass/Appetizer.java
@@ -1,15 +1,13 @@
 package entityclass;
 
-import java.io.Serializable;
+public class Appetizer extends MenuItem {
 
-public class Appetizer extends MenuItem implements Serializable {
-
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -7202427746854594849L;
 
 	public Appetizer(String appeName, double appePrice, String appeDescription, String appeID) {
-        super(appeName,appePrice,appeDescription,appeID);
-    }
+		super(appeName, appePrice, appeDescription, appeID);
+	}
 }
