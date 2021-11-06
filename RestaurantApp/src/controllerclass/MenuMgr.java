4fa4ed4680ae5b53603c8a2821ac9e9b56d98917
WijayaTimothyLarry <wijayatimothylarry@gmail.com>
2021-11-06 11:44:42 +0800

    make menumgr static

--- RestaurantApp/src/controllerclass/MenuMgr.java
+++ RestaurantApp/src/controllerclass/MenuMgr.java
@@ -10,80 +10,80 @@ public class MenuMgr {
         restaurantMenu.printAppeList();
     }
 
-    public void showMainCourse() {
+    public static void showMainCourse() {
         restaurantMenu.printMainCourse();
     }
 
-    public void showDessert() {
+    public static void showDessert() {
         restaurantMenu.printDessert();
     }
 
-    public void showDrinks() {
+    public static void showDrinks() {
         restaurantMenu.printDrinks();
     }
 
-    public void showPackage() {
+    public static void showPackage() {
         restaurantMenu.printPackage();
     }
 
     // Functions to edit menu
     // 1) Adding into menu
     // Appetizer
-    public void addAppe(Appetizer appe) {
+    public static void addAppe(Appetizer appe) {
         restaurantMenu.addintoAppeList(appe);
     }
 
     // Main Course
-    public void addMainCourse(MainCourse maincourse) {
+    public static void addMainCourse(MainCourse maincourse) {
         restaurantMenu.addintoMainCourseList(maincourse);
     }
 
     // Dessert
-    public void addDessert(Dessert dessert) {
+    public static void addDessert(Dessert dessert) {
         restaurantMenu.addintoDessertList(dessert);
     }
 
     // Drinks
-    public void addDrinks(Drinks drinks) {
+    public static void addDrinks(Drinks drinks) {
         restaurantMenu.addintoDrinksList(drinks);
     }
 
     // Package
-    public void addPackage(PackageItem packages) {
+    public static void addPackage(PackageItem packages) {
         restaurantMenu.addintoPackageList(packages);
     }
 
     // 2) Delete item
     // Appetizer
-    public void removeAppe(String id) {
+    public static void removeAppetizer(String id) {
         if (restaurantMenu.removeforAppeList(id) == -1) {
             System.out.println("Item not found");
         }
     }
 
     // MainCourse
-    public void removeMainCourse(String id) {
+    public static void removeMainCourse(String id) {
         if (restaurantMenu.removeforMainCourseList(id) == -1) {
             System.out.println("Item not found");
         }
     }
 
     // Dessert
-    public void removeDessert(String id) {
+    public static void removeDessert(String id) {
         if (restaurantMenu.removeforDessertList(id) == -1) {
             System.out.println("Item not found");
         }
     }
 
     // Drinks
-    public void removeDrinks(String id) {
+    public static void removeDrinks(String id) {
         if (restaurantMenu.removeforDrinksList(id) == -1) {
             System.out.println("Item not found");
         }
     }
 
     // Packages
-    public void removePackage(String id) {
+    public static void removePackage(String id) {
         if (restaurantMenu.removeforPackageList(id) == -1) {
             System.out.println("Item not found");
         }
