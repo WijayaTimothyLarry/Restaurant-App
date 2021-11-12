2f7f675d8af60724a8e33b22f9f2a6efade34b9e
svenkneegear <phengkhai@gmail.com>
2021-11-12 15:04:10 +0800

    More Javadoc

--- RestaurantApp/src/boundaryclass/MenuInterface.java
+++ RestaurantApp/src/boundaryclass/MenuInterface.java
@@ -18,6 +18,12 @@ public class MenuInterface {
 
     /**
      * Function to show all front-end options available for user to choose for them to edit Menu
+     * (1)Display a menu of their choice
+     * (2)Add an item to a list of their choice
+     * (3)Remove an item from a list of their choice
+     * (4)Update an item from a list of their choice
+     * (0)Exit the program
+     * @see MenuMgr
      */
     public static void showOption() {
         boolean on = true;
@@ -57,6 +63,13 @@ public class MenuInterface {
     /**
      * Function for user to choose which list to display
      * Call corresponding show functions from MenuMgr
+     * Input result:
+     * (1) Display Appetizer list
+     * (2) Display Main Course List
+     * (3) Display Dessert List
+     * (4) Display Drinks List
+     * (5) Display Promo Pacakge List
+     * (0) Go back to the previous menu
      * @see MenuMgr
      */
     private static void displayOption() {
@@ -98,6 +111,13 @@ public class MenuInterface {
     /**
      * Function to add new MenuItem into a list of users choice
      * Call corresponding function from MenuMgr
+     * Input corresponds to:
+     * (1)Add a new item to Appetizer list
+     * (2)Add a new item to Main Course list
+     * (3)Add a new item to Dessert list
+     * (4)Add a new item to Drinks List
+     * (5)Add a new item to PackageItem list
+     * (0)Go back to the previous menu
      * @see MenuMgr
      */
     private static void addNewMenuOption() {
@@ -203,6 +223,12 @@ public class MenuInterface {
     /**
      * Function to remove an existing MenuItem from a list of user's choice
      * Call corresponding function from MenuMgr
+     * Input corresponds to
+     * (1)Remove an item from Appetizer list
+     * (2)Remove an item from Main Course list
+     * (3)Remove an item from Dessert list
+     * (4)Remove an item from PackageItem list
+     * (0)Go back to the previous menu
      * @see MenuMgr
      */
     private static void removeMenuOption() {
@@ -257,6 +283,19 @@ public class MenuInterface {
     /**
      * Function for user to update a MenuItem from the list of their choice
      * Call corresponding function from MenuMgr
+     * Index corresponds to
+     * (1)Update an item in Appetizer List
+     * (2)Update an item in Main Course list
+     * (3)Update an item in Dessert List
+     * (4)Update an item in Drinks list
+     * (5)Update an item in PackageItem list
+     * (0)Go back to the previous menu
+     * After a valid input is made, user will have to  choose with item they want to change by ID and 
+     * will be prompted to make another input on which attributes of the MenuItem do they want to change
+     * Choice includes:
+     * (1)Change Name of the corresponding MenuItem
+     * (2)Change Price of the corresponding MenuItem
+     * (3)Change Description of the corresponding MenuItem
      * @see MenuMgr
      */
 
@@ -497,6 +536,7 @@ public class MenuInterface {
      * @param description
      * @param id
      * @return new PackageItem created
+     * @see PackageItem
      */
     private static PackageItem createPackage(String name, double price, String description, String id) {
         Drinks packageDrinks = null;
